5a7e1122fa19550c3c5aff30ddb23e7bf8db12c1
raorao <savinirs@gmail.com>
2018-06-20 20:07:15 -0700

    remove boilerplate test

--- apps/ynab_loader/lib/ynab_loader.ex
+++ apps/ynab_loader/lib/ynab_loader.ex
@@ -1,18 +1,2 @@
 defmodule YNABLoader do
-  @moduledoc """
-  Documentation for YNABLoader.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> YNABLoader.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/ynab_loader/test/ynab_loader_test.exs
+++ apps/ynab_loader/test/ynab_loader_test.exs
@@ -1,8 +1,4 @@
 defmodule YNABLoaderTest do
   use ExUnit.Case
   doctest YNABLoader
-
-  test "greets the world" do
-    assert YNABLoader.hello() == :world
-  end
 end
